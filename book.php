ce5f65fd7f8427c615126e40cb41e857499f19c9
IsaacN <me@isaacnankavill.com>
2016-03-28 12:25:27 +1000

    Dynamic Templating and Book Page

---
+++ book.php
@@ -0,0 +1,167 @@
+<? include('engine/head.php'); include('engine/nav.php'); ?>
+		<div class="main-container">
+		<section>
+		        <div class="container">
+		            <div class="row">
+		                <div class="col-sm-12 text-center">
+		                    <h4 class="uppercase mb16">Extended Plans</h4>
+		                    <p class="lead mb64">
+		                        Perfect for plans with greater detail. Alter appearance with '.boxed' and '.emphasis' classes
+		                    </p>
+		                </div>
+		            </div>
+		            
+		            <div class="row">
+		                <div class="col-md-4 col-sm-6">
+		                    <div class="pricing-table pt-2 text-center">
+		                        <h5 class="uppercase">TRX Workout</h5>
+		                        <span class="price">$99</span>
+		                        <p class="lead">Per Week</p>
+		                        <a class="btn btn-filled btn-lg" href="#">Get Started</a>
+		                        <ul>
+		                            <li>
+		                                <strong>Unlimited</strong> access to content
+		                            </li>
+		                            <li>
+		                                <strong>Fully Secure</strong> online backup
+		                            </li>
+		                            <li>
+		                                <strong>One Year</strong> round the clock support
+		                            </li>
+		                            <li>
+		                                <strong>FREE</strong> complimentary lanyard
+		                            </li>
+		                        </ul>
+		                    </div>
+		                    
+		                </div>
+		                <div class="col-md-4 col-sm-6">
+		                    <div class="pricing-table pt-2 boxed text-center">
+		                        <h5 class="uppercase">Weight Loss</h5>
+		                        <span class="price">$99</span>
+		                        <p class="lead">Per Week</p>
+		                        <a class="btn btn-filled btn-lg" href="#">Get Started</a>
+		                        <ul>
+		                            <li>
+		                                <strong>Unlimited</strong> access to content
+		                            </li>
+		                            <li>
+		                                <strong>Fully Secure</strong> online backup
+		                            </li>
+		                            <li>
+		                                <strong>One Year</strong> round the clock support
+		                            </li>
+		                            <li>
+		                                <strong>FREE</strong> complimentary lanyard
+		                            </li>
+		                        </ul>
+		                    </div>
+		                    
+		                </div>
+		                <div class="col-md-4 col-sm-6">
+		                    <div class="pricing-table pt-2 emphasis text-center">
+		                        <h5 class="uppercase">Muscle Tone</h5>
+		                        <span class="price">$99</span>
+		                        <p class="lead">Per Week</p>
+		                        <a class="btn btn-white btn-lg" href="#">Get Started</a>
+		                        <ul>
+		                            <li>
+		                                <strong>Unlimited</strong> access to content
+		                            </li>
+		                            <li>
+		                                <strong>Fully Secure</strong> online backup
+		                            </li>
+		                            <li>
+		                                <strong>One Year</strong> round the clock support
+		                            </li>
+		                            <li>
+		                                <strong>FREE</strong> complimentary lanyard
+		                            </li>
+		                        </ul>
+		                    </div>
+		                    
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		    </section><section>
+		        <div class="container">
+		            
+		            
+		            <div class="row">
+		                <div class="col-md-4 col-sm-6">
+		                    <div class="pricing-table pt-2 text-center">
+		                        <h5 class="uppercase">Posture Correction</h5>
+		                        <span class="price">$99</span>
+		                        <p class="lead">Per Week</p>
+		                        <a class="btn btn-filled btn-lg" href="#">Get Started</a>
+		                        <ul>
+		                            <li>
+		                                <strong>Unlimited</strong> access to content
+		                            </li>
+		                            <li>
+		                                <strong>Fully Secure</strong> online backup
+		                            </li>
+		                            <li>
+		                                <strong>One Year</strong> round the clock support
+		                            </li>
+		                            <li>
+		                                <strong>FREE</strong> complimentary lanyard
+		                            </li>
+		                        </ul>
+		                    </div>
+		                    
+		                </div>
+		                <div class="col-md-4 col-sm-6">
+		                    <div class="pricing-table pt-2 boxed text-center">
+		                        <h5 class="uppercase">Cardio Fitness</h5>
+		                        <span class="price">$99</span>
+		                        <p class="lead">Per Week</p>
+		                        <a class="btn btn-filled btn-lg" href="#">Get Started</a>
+		                        <ul>
+		                            <li>
+		                                <strong>Unlimited</strong> access to content
+		                            </li>
+		                            <li>
+		                                <strong>Fully Secure</strong> online backup
+		                            </li>
+		                            <li>
+		                                <strong>One Year</strong> round the clock support
+		                            </li>
+		                            <li>
+		                                <strong>FREE</strong> complimentary lanyard
+		                            </li>
+		                        </ul>
+		                    </div>
+		                    
+		                </div>
+		                <div class="col-md-4 col-sm-6">
+		                    <div class="pricing-table pt-2 emphasis text-center">
+		                        <h5 class="uppercase">Core Strength</h5>
+		                        <span class="price">$99</span>
+		                        <p class="lead">Per Week</p>
+		                        <a class="btn btn-white btn-lg" href="#">Get Started</a>
+		                        <ul>
+		                            <li>
+		                                <strong>Unlimited</strong> access to content
+		                            </li>
+		                            <li>
+		                                <strong>Fully Secure</strong> online backup
+		                            </li>
+		                            <li>
+		                                <strong>One Year</strong> round the clock support
+		                            </li>
+		                            <li>
+		                                <strong>FREE</strong> complimentary lanyard
+		                            </li>
+		                        </ul>
+		                    </div>
+		                    
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		    </section></div>
+	<? include('engine/footer.php');?>
--- index.php
+++ index.php
@@ -1,117 +1,9 @@
-
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Elite Four Fitness</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
-        <link href="css/themify-icons.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/flexslider.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/lightbox.min.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/ytplayer.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/theme-red.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
-        <link href='http://fonts.googleapis.com/css?family=Lato:300,400%7CRaleway:100,400,300,500,600,700%7COpen+Sans:400,500,600' rel='stylesheet' type='text/css'>
-    </head>
-    <body>
-				
-		<div class="nav-container">
-		    
-		    
-		
-		    
-		    <nav>
-		        <div class="nav-bar">
-		            <div class="module left">
-		                <a href="index.html">
-		                    <img class="logo logo-light" alt="Foundry" src="img/logo-light.png">
-		                    <img class="logo logo-dark" alt="Foundry" src="img/e4f-logo-compressed-long.png">
-		                </a>
-		            </div>
-		            <div class="module widget-handle mobile-toggle right visible-sm visible-xs">
-		                <i class="ti-menu"></i>
-		            </div>
-		            <div class="module-group right">
-		                <div class="module left">
-		                    <ul class="menu">
-		                        <li>
-		                            <a href="#">Single</a>
-		                        </li>
-		                        <li class="has-dropdown">
-		                            <a href="#">
-		                                Mega Menu
-		                            </a>
-		                            <ul class="mega-menu">
-		                                <li>
-		                                    <ul>
-		                                        <li>
-		                                            <span class="title">Column 1</span>
-		                                        </li>
-		                                        <li>
-		                                            <a href="#">Single</a>
-		                                        </li>
-		                                    </ul>
-		                                </li>
-		                                <li>
-		                                    <ul>
-		                                        <li>
-		                                            <span class="title">Column 2</span>
-		                                        </li>
-		                                        <li>
-		                                            <a href="#">Single</a>
-		                                        </li>
-		                                    </ul>
-		                                </li>
-		                            </ul>
-		                        </li>
-		                        <li class="has-dropdown">
-		                            <a href="#">
-		                                Single Dropdown
-		                            </a>
-		                            <ul>
-		                                <li class="has-dropdown">
-		                                    <a href="#">
-		                                        Second Level
-		                                    </a>
-		                                    <ul>
-		                                        <li>
-		                                            <a href="#">
-		                                                Single
-		                                            </a>
-		                                        </li>
-		                                    </ul>
-		                                </li>
-		                            </ul>
-		                        </li>
-		                    </ul>
-		                </div>
-		                
-		                <div class="module widget-handle language left">
-		                    <ul class="menu">
-		                        <li class="has-dropdown">
-		                            <a href="#">ENG</a>
-		                            <ul>
-		                                <li>
-		                                    <a href="#">French</a>
-		                                </li>
-		                                <li>
-		                                    <a href="#">Deutsch</a>
-		                                </li>
-		                            </ul>
-		                        </li>
-		                    </ul>
-		                </div>
-		            </div>
-		            
-		        </div>
-		    </nav>
-		
-		</div>
-		
+<?php
+	$pageTitle = 'Home';
+	include('engine/head.php');
+	include('engine/nav.php');
+?>
 		<div class="main-container">
-					
 			<section class="image-bg overlay pt240 pb240 pt-xs-180 pb-xs-180">
 				<div class="background-image-holder">
 					<img alt="image" class="background-image" src="img/fitness3.jpg">
@@ -119,7 +11,7 @@
 				<div class="container">
 					<div class="row">
 						<div class="col-sm-6">
-							<h1 class="mb0 uppercase bold italic">Elite Four Fitness<br></h1>
+							<h1 class="mb0 uppercase bold italic">Elite Four Fitness <br></h1>
 							<h5 class="uppercase mb32">Personal Training &amp; Dietary Advice</h5>
 							<p class="lead mb0">
 								“Everyone deserves good health and happiness.<br class="hidden-sm">My goal is to help people achieve both.”
@@ -289,46 +181,7 @@
 		            
 		        </div>
 		        
-		    </section><footer class="footer-2 bg-dark text-center-xs">
-				<div class="container">
-					<div class="row">
-						<div class="col-sm-4">
-							<a href="#"><div class="vnu"><img class="image-xxs fade-half" alt="Pic" src="img/e4f-logo-compressed-long.png"></div></a>
-						</div>
-					
-						<div class="col-sm-4 text-center">
-							<span class="fade-half">
-								© Copyright 2016 Elite Four Fitness - All Rights Reserved
-							</span>
-						</div>
-					
-						<div class="col-sm-4 text-right text-center-xs">
-							<ul class="list-inline social-list">
-								<li><a href="#"><i class="ti-twitter-alt"></i></a></li>
-								<li><a href="#"><i class="ti-facebook"></i></a></li>
-								<li><a href="#"><i class="ti-dribbble"></i></a></li>
-								<li><a href="#"><i class="ti-vimeo-alt"></i></a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</footer>
-		</div>
-		
-				
-	<script src="js/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/flexslider.min.js"></script>
-        <script src="js/lightbox.min.js"></script>
-        <script src="js/masonry.min.js"></script>
-        <script src="js/flickr.js"></script>
-        <script src="js/twitterfetcher.min.js"></script>
-        <script src="js/spectragram.min.js"></script>
-        <script src="js/ytplayer.min.js"></script>
-        <script src="js/countdown.min.js"></script>
-        <script src="js/smooth-scroll.min.js"></script>
-        <script src="js/parallax.js"></script>
-        <script src="js/scripts.js"></script>
-    </body>
-</html>
-				
+		    </section>
+<? 
+	include('engine/footer.php');
+?>
