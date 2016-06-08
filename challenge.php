d8797bc24d34a1193f008d3e8983d74f71588f83
IsaacN <me@isaacnankavill.com>
2016-06-08 13:50:03 +1000

    Lead page initial

---
+++ challenge.php
@@ -0,0 +1,294 @@
+<?php
+	$pageTitle = 'Home';
+	include('engine/head.php');
+	include('engine/nav.php');
+?>
+		
+<!doctype html>
+			
+		<section class="image-bg overlay pt240 pb240 pt-xs-180 pb-xs-180">
+				<div class="background-image-holder">
+					<img alt="image" class="background-image" src="img/hayden-hero.jpg">
+				</div>
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-6">
+							<h1 class="mb0 uppercase bold italic">Next Level Challenge.<br></h1>
+							<h5 class="uppercase mb32">Get the results you dream of in just six weeks.</h5>
+							<p class="lead mb0">
+								
+							</p>
+						</div>
+					</div>
+					
+				</div>
+				
+			</section>
+		<section class="bg-primary">
+		        <div class="container">
+		            <div class="row">
+		                <div class="col-sm-8 col-sm-offset-2 text-center">
+		                    <h3 class="mb64 uppercase">People &nbsp;
+		                        <i class="ti-heart"></i>&nbsp; NLC</h3>
+		                    <div class="testimonials text-slider slider-arrow-controls">
+		                        <ul class="slides">
+		                            <li>
+		                                <p class="lead">This is a quote praising the Next Level Challenge and all the stuff it does. I loved the NLC because of the things and also the stuff as well as the other things.</p>
+		                                <div class="quote-author">
+		                                    <img alt="Avatar" src="img/avatar1.png">
+		                                    <h6 class="uppercase">Anna Thompson</h6>
+		                                    <span>Elite Four Fitness Client</span>
+		                                </div>
+		                            </li>
+		                            <li>
+		                               <p class="lead">This is a SECOND quote praising the Next Level Challenge and all the stuff it does. I loved the NLC because of the things and also the stuff as well as the other things.</p>
+		                                <div class="quote-author">
+		                                    <img alt="Avatar" src="img/avatar2.png">
+		                                    <h6 class="uppercase">Rick Dempsey</h6>
+		                                    <span>Elite Four Fitness Client</span>
+		                                </div>
+		                            </li>
+		                            <li>
+		                                <p class="lead">This is the THIRD quote praising the Next Level Challenge and all the stuff it does. I loved the NLC because of the things and also the stuff as well as the other things.</p>
+		                                <div class="quote-author">
+		                                    <img alt="Avatar" src="img/avatar3.png">
+		                                    <h6 class="uppercase">Gill Sans</h6>
+		                                    <span>Elite Four Fitness Client</span>
+		                                </div>
+		                            </li>
+		                        </ul>
+		                    </div>
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		    </section><section class="image-bg bg-light parallax overlay pt160 pb160 pt-xs-80 pb-xs-80">
+		        <div class="background-image-holder">
+		            <img alt="image" class="background-image" src="img/intro1.jpg">
+		        </div>
+		        <div class="container">
+		            <div class="row">
+		                <div class="col-md-7 col-sm-8">
+		                    <i class="ti-quote-left icon icon-sm mb16"></i>
+		                    <h3 class="mb32">Foundry provided us the tools, capital and expertise we needed to launch Lactick to reach a global audience.</h3>
+		                    <p>
+		                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+		                    </p>
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		    </section><section class="pb64 pb-xs-40">
+		        <div class="container">
+		            <div class="row">
+		                <div class="col-sm-4">
+		                    <div class="feature feature-2 filled text-center">
+		                        <div class="text-center">
+		                            <i class="ti-layers icon-sm"></i>
+		                            <h5 class="uppercase">Modular Design</h5>
+		                        </div>
+		                        <p>
+		                            With a plethora of purpose-built content blocks, colors and fonts, Foundry presents a mind-boggling number of combinations. Test drive the builder now!
+		                        </p>
+		                    </div>
+		                    
+		                </div>
+		                <div class="col-sm-4">
+		                    <div class="feature feature-2 filled text-center">
+		                        <div class="text-center">
+		                            <i class="ti-gallery icon-sm"></i>
+		                            <h5 class="uppercase">Silky Parallax</h5>
+		                        </div>
+		                        <p>
+		                            We've built a buttery smooth parallax scrolling experience with a heavy focus on performance. Extensively tested across a range of browser and mouse types.
+		                        </p>
+		                    </div>
+		                    
+		                </div>
+		                <div class="col-sm-4">
+		                    <div class="feature feature-2 filled text-center">
+		                        <div class="text-center">
+		                            <i class="ti-package icon-sm"></i>
+		                            <h5 class="uppercase">Unique Concepts</h5>
+		                        </div>
+		                        <p>
+		                            10 Fresh and unique concepts included out of the box. From portfolio to property showcase, Foundry's adaptable look is perfect for your next project.
+		                        </p>
+		                    </div>
+		                    
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		    </section><section>
+		        <div class="container">
+		            <div class="row">
+		                <div class="col-sm-12 text-center">
+		                    <h4 class="uppercase mb16">Brief Plans</h4>
+		                    <p class="lead mb64">
+		                        Perfect for uncomplicated pricing options. Alter appearance with '.boxed' and '.emphasis' classes
+		                    </p>
+		                </div>
+		            </div>
+		            
+		            <div class="row">
+		                <div class="col-md-4 col-sm-6">
+		                    <div class="pricing-table pt-1 text-center">
+		                        <h5 class="uppercase">Starter</h5>
+		                        <span class="price">$12</span>
+		                        <p class="lead">Per Month</p>
+		                        <a class="btn btn-filled btn-lg" href="#">Get Started</a>
+		                        <p>
+		                            30 Day Free Trial
+		                            <br> No Credit Card Required
+		                        </p>
+		                    </div>
+		                    
+		                </div>
+		                <div class="col-md-4 col-sm-6">
+		                    <div class="pricing-table pt-1 text-center boxed">
+		                        <h5 class="uppercase">Starter</h5>
+		                        <span class="price">$12</span>
+		                        <p class="lead">Per Month</p>
+		                        <a class="btn btn-filled btn-lg" href="#">Get Started</a>
+		                        <p>
+		                            30 Day Free Trial
+		                            <br> No Credit Card Required
+		                        </p>
+		                    </div>
+		                    
+		                </div>
+		                <div class="col-md-4 col-sm-6">
+		                    <div class="pricing-table pt-1 text-center emphasis">
+		                        <h5 class="uppercase">Starter</h5>
+		                        <span class="price">$12</span>
+		                        <p class="lead">Per Month</p>
+		                        <a class="btn btn-white btn-lg" href="#">Get Started</a>
+		                        <p>
+		                            30 Day Free Trial
+		                            <br> No Credit Card Required
+		                        </p>
+		                    </div>
+		                    
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		    </section><section>
+		        <div class="container">
+		            <div class="row v-align-children">
+		                <div class="col-sm-5">
+		                    <h3>Join us for a day of
+		                        <br> ideas &amp; discussion.</h3>
+		                    <p class="lead mb40">
+		                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+		                    </p>
+		                    <div class="overflow-hidden mb32 mb-xs-24">
+		                        <i class="ti-package icon icon-sm pull-left"></i>
+		                        <h6 class="uppercase mb0 inline-block p32">All Inclusive Package</h6>
+		                    </div>
+		                    <div class="overflow-hidden mb32 mb-xs-24">
+		                        <i class="ti-medall-alt icon icon-sm pull-left"></i>
+		                        <h6 class="uppercase mb0 inline-block p32">Foundry Club Access</h6>
+		                    </div>
+		                </div>
+		                <div class="col-md-4 col-sm-6">
+		                    <div class="pricing-table pt-1 text-center emphasis">
+		                        <h5 class="uppercase">Admit One</h5>
+		                        <span class="price">$89</span>
+		                        <p class="lead">Per Ticket</p>
+		                        <a class="btn btn-white btn-lg" href="#">Purchase Ticket</a>
+		                        <p>
+		                            <a href="#">Contact Us for</a>
+		                            <br> large ticket volumes
+		                        </p>
+		                    </div>
+		                    
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		    </section><section class="bg-dark pt64 pb64">
+		        <div class="container">
+		            <div class="row">
+		                <div class="col-sm-12 text-center">
+		                    <h2 class="mb8">Start A Foundry Project</h2>
+		                    <p class="lead mb40 mb-xs-24">
+		                        Variant Page Builder, Over 100 Page Templates - The choice is clear.
+		                    </p>
+		                    <a class="btn btn-filled btn-lg mb0" href="#purchase-template">Purchase Foundry</a>
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		    </section><section>
+		        <div class="container">
+		            <div class="row">
+		                <div class="col-sm-10 col-sm-offset-1 text-center">
+		                    <h3 class="uppercase color-primary mb40 mb-xs-24">A Place To Call Home</h3>
+		                    <p class="lead">
+		                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
+		                    </p>
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		    </section></div>
+		
+				
+	<script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/flexslider.min.js"></script>
+        <script src="js/lightbox.min.js"></script>
+        <script src="js/masonry.min.js"></script>
+        <script src="js/flickr.js"></script>
+        <script src="js/twitterfetcher.min.js"></script>
+        <script src="js/spectragram.min.js"></script>
+        <script src="js/ytplayer.min.js"></script>
+        <script src="js/countdown.min.js"></script>
+        <script src="js/smooth-scroll.min.js"></script>
+        <script src="js/parallax.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+				
+		    <section id='contact'>
+		        <div class="container">
+		            <div class="row">
+		                <div class="col-sm-6 col-md-5">
+		                    <h4 class="uppercase">Get In Touch</h4>
+		                    <p>
+		                        If you feel like finding out a bit more about what we do at Elite Four Fitness fill in your name, email address and a brief message about how we could help you and we will get back to you ASAP to book in a free consultation.
+		                    </p>
+		                    <hr>
+		                    <p>Hayden Walker <br style='display: inline-block;'>Snap Fitness Victoria Point <br style='display: inline-block;'>Town Centre Shopping Centre, Victoria Point <br style='display: inline-block;'>Queensland 4165 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<br></p>
+		                    <hr>
+		                    <p>
+		                        <strong>E:</strong>&nbsp;hayden@elitefourfitness.com&nbsp;<br>
+		                        <strong>P:</strong> 0422 397 266 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<br>
+		                    </p>
+		                </div>
+		                <div class="col-sm-6 col-md-5 col-md-offset-1">
+		                    <form class="form-email" data-success="Thanks for your submission, we will be in touch shortly." data-error="Please fill all fields correctly.">
+		                        <input type="text" class="validate-required" name="name" placeholder="Your Name">
+		                        <input type="text" class="validate-required" name="phone" placeholder="Phone Number">
+		                        <input type="text" class="validate-required validate-email" name="email" placeholder="Email Address">
+		                        <textarea class="validate-required" name="message" rows="4" placeholder="Message"></textarea>
+		                        <button type="submit">Send Message</button>
+		                    </form>
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		    </section>
+<? 
+	include('engine/footer.php');
+?>
